692eaa5e8b97e4f7a3ab6f7b741047f4fa2d929b
bieleiji <146545505+bieleiji@users.noreply.github.com>
2026-05-09 21:06:46 -0300

    adição de decodificação em base 64

--- src/main/java/com/example/controleestoquespring/Service/JwtService.java
+++ src/main/java/com/example/controleestoquespring/Service/JwtService.java
@@ -2,6 +2,7 @@ package com.example.controleestoquespring.Service;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,16 +15,23 @@ public class JwtService {
     @Value("${jwt.secret}")
     private String secret;
 
+    @Value("${jwt.expiration}")
+    private long expiration;
+
     public String gerarToken(String nome) {
         return Jwts.builder()
                 .setSubject(nome)
+                .claim("role","ADMIN")
+                .claim("id", 1)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 *60))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
                 .signWith(getKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public Key getKey() {
-        return Keys.hmacShaKeyFor(secret.getBytes());
+        byte[] keybytes = Decoders.BASE64.decode(secret);
+
+        return Keys.hmacShaKeyFor(keybytes);
     }
 }
